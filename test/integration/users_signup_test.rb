1e80e594a59919c629d94dac627ef92042770326
aichane <anael.ichane@gmail.com>
2015-08-23 13:57:38 +0200

    Exercice

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -9,6 +9,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 		                        password:              "foo",
 		                        password_confirmation: "bar" }
 		end
-		assert_template 'users/new'		
+		assert_template 'users/new'
+		
+
 	end
 end
